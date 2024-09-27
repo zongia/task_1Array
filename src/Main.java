9e28570243264bfaf4edd3abadf369b7c69c23f9
zongia <firatkalkan023@gmail.com>
2024-09-27 13:16:07 +0300

    Arraylist

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Random;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -5,48 +6,47 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         Random s =new Random();
+        ArrayList list = new ArrayList();
+        ArrayList list1 = new ArrayList();
 
-        int[] list=new int[100];
 
-        int[] list_kopya=new int[100];
+
+
+
 
         int op=0;
 
 
 
-        while(op<list.length)
+        while(op<100)
         {
 
-            int sayı=s.nextInt(1000);
+            int sayi=s.nextInt(1000);
+            list.add(sayi);
+            op++;
 
 
-            for(int i=0;i<list.length;i++)
-            {
-                if(list[i]==sayı) {	}
-                else
-                {	list[op]=sayı;op++;
-                }
 
-            }
+
         }
+        list1.addAll(list);
+        int sayi=s.nextInt(list.size());
+        list1.remove(sayi);
 
 
 
 
-        for(int y=0;y<list.length;y++) {
-            list_kopya[y]=list[y];
-        }
-        int p=s.nextInt(100);
-        list_kopya[p]=-1;
 
 
-        for(int y=0;y<list.length;y++)
+
+        for(int y=0;y<list.size();y++)
         {
-            if(list[y]==list_kopya[y]) {}
+            if(list.get(y)==list1.get(y)) {}
             else
             {
 
-                System.out.println("eksik olan elaman  =="+(y+1) );//index değil kaçıncı sayı oldunu yazdım
+                System.out.println("eksik olan elaman 2 =="+(y+1) );//index değil kaçıncı sayı oldunu yazdım
+                System.out.println("eksik olan sayı "+list.get(y));
                 break;
 
             }
